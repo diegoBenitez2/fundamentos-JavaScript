94267b565da1d0167f783a67f01ccc48160956ea
diegoBenitez <diegof.17@hotmail.com>
2020-01-17 22:01:03 -0500

    comparaciones en js

---
+++ comparaciones_js.js
@@ -0,0 +1,26 @@
+var x = 4, y ='4'
+//comparar con == a un mismo tipo de dato
+x==y
+// comparar con tripe igual(===)
+x===y
+
+
+var diego =  {
+  nombre:'diego',
+}
+
+var julio ={
+  nombre: 'julio'
+}
+
+//COMPARAR OBJETOS
+//preguntar por referencia a la variabale que utilizamos
+diego == julio
+//es falso porque estan en difrentes espacio en RAM
+
+//aqui apunta al mismo espacio en memoria
+var otraPersona = diego
+//dará true
+
+
+
