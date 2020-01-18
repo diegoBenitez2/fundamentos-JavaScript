1b0544026ee3e09342a66a10cf84819e09411888
diegoBenitez <diegof.17@hotmail.com>
2020-01-17 21:40:10 -0500

    objetos en js

---
+++ objetos.js
@@ -0,0 +1,60 @@
+//OBJETOS EN JAVASCRIPT
+
+//Objeto que tiene clave y valor
+//Clave(Key): numeros, string
+//Valor: numericos, string 
+var diego = {
+  nombre:'diego',
+  apellido:'Benitez',
+  edad: 28
+}
+//se desestructura el objeto con {} asignando el valor del atributo
+// function imprimirMayuscula({ nombre }){
+//   //la variable definida como parametro  'nombre' existe en alcance local(de la funcion)
+//  nombre = nombre.toUpperCase()
+//   console.log(nombre)
+// }
+// //modifico el valor de ese atributo del objeto
+// imprimirMayuscula({nombre :'dario'})
+
+// //OTRA MANERA
+// //ENVIANDO TODO EL OBJETO
+function imprimirMayuscula(persona){
+//  var  nombre = persona.nombre.toUpperCase()
+//   console.log(nombre)
+//  ESTE ES UNA FORMA MAS CORTA
+ var { nombre } = persona
+//  IMPRIME ASI:
+ console.log(nombre.toUpperCase())
+
+}
+imprimirMayuscula(diego)
+
+//DESESTRUCTURA EL OBJETO ENVIANDO SOLO EL ATRIBUTO
+
+// function imprimirMayuscula(nombre){
+//  nombre = nombre.toUpperCase()
+//   console.log(nombre)
+// }
+// imprimirMayuscula(persona.nombre)
+
+//ENVIAR PARAMETROS DE OBJETOS COMO REFERENCIA
+//le pasamos el OBJETO en la funcion
+// function cumpleanos(persona){
+// persona.edad += 1
+// }
+//le pasamos un TATRIBUTO del objeto  en la funcion
+// function cumpleanos(edad){
+//   edad +=1
+//   console.log(edad)
+// }
+//crearmos un nuevo objeto copiando la referencia del objeto
+function cumpleanos(persona){
+  return{
+    ...persona,
+    edad: persona.edad +  1,
+  }
+}
+var diegoMasViejo = cumpleanos(diego)
+console.log(diegoMasViejo)
+
