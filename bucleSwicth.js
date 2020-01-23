7501fe09d76c5805f72bc7e041a81cbe7cf1146c
diegoBenitez <diegof.17@hotmail.com>
2020-01-23 10:55:23 -0500

    bucles: while doWhile, swicth, array1

---
+++ bucleSwicth.js
@@ -0,0 +1,24 @@
+var signo = prompt('Cual es tu signo');
+
+switch(signo){
+  case 'acuario' :
+  case 'acúario': 
+  console.log('Estás iniciando un ciclo muy diferente a lo que esperabas, pero te sentirás bien con todo lo que estás cosechando en el área sentimental.');
+  break
+
+  case 'piscis':
+    console.log('¿Ves cómo los problemas no eran tan graves y tenían solución? Nuevamente la luz empieza a brillar en tu horizonte amoroso. A partir de este momento todo cambia para bien. Las situaciones que te causaron desasosiego y nostalgia quedarán en el pasado. Vive en el sueño del mañana, pero en ningún momento dejes de disfrutar del hoy.');
+  break
+
+  case 'libra':
+    console.log('No importa lo que pase, librano, tú sabrás transformar cualquier situación negativa en positiva. Tu sentido del humor contribuirá mucho a tu felicidad de hoy.Si hubo algún problema entre los dos una broma sana romperá el hielo y permitirá nuevamente la reconciliación amorosa. Concédete tiempo para reflexionar mejor tus decisione');
+  break
+
+case 'tauro':
+    console.log('Estás iniciando un ciclo muy diferente a lo que esperabas, pero te sentirás bien con todo lo que estás cosechando en el área sentimental.');
+  break
+
+  default: 
+  console.log('no es un  signo zodiacal valido')
+  break
+}
---
+++ bucleWhile.js
@@ -0,0 +1,42 @@
+var juan = {
+  nombre: 'juan',
+  apellido:'Gomez',
+  edad: 27,
+  peso: 75,
+}
+const META = juan.peso - 3;
+const INCREMENTO = 0.2;
+const  aumentardePeso = persona => persona.peso += INCREMENTO_PESO
+const   adelgazar = persona => persona.peso -= INCREMENTO_PESO
+var dias = 0
+while(juan.peso > META){
+  if(Math.random()< 0.3){
+    aumentardePeso(juan)
+  }
+  if(Math.random()< 0.4){
+    adelgazar(juan)
+  }
+  dias +=  1
+}
+
+console.log(`Le tomo ${dias} dias a ${juan.nombre} bajar 3Kg`)
+
+//CONDICIONALES DO WHILE
+var contador = 0;
+const llueve = () => Math.random() < 0.25;
+
+do {
+  contador ++
+}
+while(!llueve())
+if(contador > 1){
+console.log(`Fui ${contador} veces hasta que llovió`);
+
+
+}
+else {
+
+console.log(`Fui ${contador} vez hasta que llovió`);
+
+}
+
